5020ea379ac3bcfbe6e8d65419e1cc2cdf77eae3
FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
2021-06-03 10:16:45 +0000

    format code

--- api/cubeupload.js
+++ api/cubeupload.js
@@ -3,40 +3,39 @@ const https = require("https");
 
 module.exports = (req, res) => {
   if (!req.query.path) {
-    return res.json({ error: "cannot find 'path' query" })
+    return res.json({ error: "cannot find 'path' query" });
   }
-  
+
   let options = {
     hostname: "u.cubeupload.com",
     port: 443,
     path: req.query.path,
     method: "GET",
     headers: {
-      'User-Agent': req.headers['User-Agent']
-    }
-  }
-  
+      "User-Agent": req.headers["User-Agent"],
+    },
+  };
+
   let resp = https.request(options, (serverRes) => {
-    let body = '';
-    
-    if (String(serverRes.headers['content-type']).indexOf('text/html') !== -1) {
-      serverRes.on('data', (chunk) => {
-        body += chunk
-      })
-      
-      serverRes.on('end', () => {
-        res.writeHead(serverRes.statusCode, serverRes.headers)
-        res.end(body)
-      })
+    let body = "";
+
+    if (String(serverRes.headers["content-type"]).indexOf("text/html") !== -1) {
+      serverRes.on("data", (chunk) => {
+        body += chunk;
+      });
+
+      serverRes.on("end", () => {
+        res.writeHead(serverRes.statusCode, serverRes.headers);
+        res.end(body);
+      });
     } else {
       serverRes.pipe(res, {
-        end: true
+        end: true,
       });
-      
-      res.contentType(serverRes.headers['content-type'])
-      
+
+      res.contentType(serverRes.headers["content-type"]);
     }
   });
-  
-  resp.end()
-}
+
+  resp.end();
+};
