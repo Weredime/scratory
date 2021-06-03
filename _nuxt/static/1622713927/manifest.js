0893d11a57fb79ee51e23f10297c05333176b7eb
FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
2021-06-03 09:52:08 +0000

    deploy: 29759895949053590d5813a6edb7118c6daeb077

---
+++ _nuxt/static/1622713927/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fspinner","\u002Fuser\u002F9gr"]})
---
+++ _nuxt/static/1622713927/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002Fscratory\u002F_nuxt\u002Fstatic\u002F1622713927",layout:"default",error:a,serverRendered:b,routePath:"\u002F",config:{_app:{basePath:"\u002Fscratory\u002F",assetsPath:"\u002Fscratory\u002F_nuxt\u002F",cdnURL:a}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
