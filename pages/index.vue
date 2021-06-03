caeb48cd64104afe4abeec0e8d656c4dbfec8561
FunctionalMetatable <67440879+FunctionalMetatable@users.noreply.github.com>
2021-06-03 12:16:57 +0100

    temporary redirect

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+</template>
+<script>
+export default {
+  head() {return {title: "Redirecting.."}},
+  async fetch() {
+    window.location.href = "/user/9gr"
+  },
+  fetchOnServer: false
+}
+</script>
