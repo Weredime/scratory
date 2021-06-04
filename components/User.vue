ac993ca1cc1dc19b48e09512c5e1fb099a29c060
FunctionalMetatable <67440879+FunctionalMetatable@users.noreply.github.com>
2021-06-04 08:52:36 +0100

    remove href

--- components/User.vue
+++ components/User.vue
@@ -17,7 +17,7 @@
           <a :href="`https://postpercent.rirurin.com/users/${username}`"
             >postpercent</a
           >
-          | <a @onclick="switchType()" href="#">View source</a>
+          | <a @onclick="switchType()">View source</a>
         </div>
       </section>
       <nav class="left-nav">
@@ -321,4 +321,6 @@ export default {
 .date {
   color: white;
 }
+
+a {cursor: pointer;}
 </style>
