2a479d748fc6fb862d825fc9022a856998d261f7
FunctionalMetatable <67440879+FunctionalMetatable@users.noreply.github.com>
2021-06-21 20:39:24 +0100

    ok

--- server-middleware/app.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const fetch = require("node-fetch");
-
-const app = express();
-
-app.get("/api/user/:user", (req, res) => {});
---
+++ server-middleware/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const fetch = require("node-fetch");
+
+const app = express();
+
+app.get("/user/:user", async (req, res) => {
+  let resp = await fetch(
+    `https://api.scratch.mit.edu/users/${req.params.user}`
+  );
+  let { id, username } = await resp.json();
+
+  res.json({
+    id,
+    username
+  });
+});
+
+app.get("/user/:user/picture", async (req, res) => {
+  let resp = await fetch(
+    `https://api.scratch.mit.edu/users/${req.params.user}`
+  );
+  let { id, username } = await resp.json();
+
+  res.redirect(`https://cdn2.scratch.mit.edu/get_image/user/${id}_500x500.png`);
+});
+
+export default app;
